04cc3b851f9d8e4045a3560eaed0594d458d59e1
Cozma <2066634@cognizant.com>
2025-05-29 16:42:37 +0300

    adding pages, styling utils and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import About from './pages/about/About';
+import Home from './pages/homepage/Home';
+import Menu from './pages/menu/Menu';
+import Reservation from './pages/reservation/Reservation';
+import ConfirmReservation from './pages/reservation/ConfirmReservation';
+import OrderOnline from './pages/order-online/OrderOnline';
+import Login from './pages/login/Login';
+import NotFound from './pages/not-found/NotFound';
 
-function App() {
+function App({ content }) {
   return (
-    <div className="App">
-      <p>Homepage</p>
-    </div>
+    <>
+      <Header />
+      {content}
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/confirmation" element={<ConfirmReservation />} />
+        <Route path="/order-online" element={<OrderOnline />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
